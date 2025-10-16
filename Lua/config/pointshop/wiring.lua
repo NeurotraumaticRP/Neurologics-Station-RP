9c0c2a9fafbcb0046edac8c3a9e48f8d1a7c403e
Austinizer <148470346+Dr-javier@users.noreply.github.com>
2025-10-16 14:13:58 -0500

    missed a nil check

--- Lua/config/pointshop/wiring.lua
+++ Lua/config/pointshop/wiring.lua
@@ -3,6 +3,7 @@ local category = {}
 category.Identifier = "wiring"
 
 category.CanAccess = function(client)
+    if not client.Character then return false end
     return client.Character and not client.Character.IsDead and client.Character.HasJob("crewmember") or client.Character.HasJob("staff")
 end
 
